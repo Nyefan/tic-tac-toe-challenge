575a972f0d4233f570debec54855d064ad78d2ff
Nyefan <nyefancoding@gmail.com>
2019-01-15 02:29:02 -0600

    Link service layer to dao layer

--- src/main/java/com/nyefan/anaconda/service/TicTacToeService.java
+++ src/main/java/com/nyefan/anaconda/service/TicTacToeService.java
@@ -1,24 +1,33 @@
 package com.nyefan.anaconda.service;
 
+import com.nyefan.anaconda.dao.TicTacToeDao;
+import com.nyefan.anaconda.dao.TicTacToeInMemoryDao;
 import com.nyefan.anaconda.data.TicTacToeBoard;
 import com.nyefan.anaconda.data.TicTacToeCreateGameRequest;
 import com.nyefan.anaconda.data.TicTacToeGame;
 import com.nyefan.anaconda.data.TicTacToeSubmitMoveRequest;
+import com.nyefan.anaconda.exception.TicTacToeException;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.UUID;
 
 public class TicTacToeService {
+
+    //TODO: inject an implementation of TicTacToeDao based on environment
+    private final TicTacToeDao dao = new TicTacToeInMemoryDao();
+
     public List<TicTacToeGame> getAllGames() {
-        return List.of(new TicTacToeGame(UUID.randomUUID(), "1", "2", new TicTacToeBoard()));
+        return dao.selectAllGames();
     }
 
     public TicTacToeGame createGame(TicTacToeCreateGameRequest createRequest) {
-        return new TicTacToeGame(UUID.randomUUID(), createRequest.getPlayerOne(), createRequest.getPlayerTwo(), new TicTacToeBoard());
+        TicTacToeGame game = new TicTacToeGame(UUID.randomUUID(), createRequest.getPlayerOne(), createRequest.getPlayerTwo(), new TicTacToeBoard());
+        return dao.upsertGame(game);
     }
 
     public TicTacToeGame getGame(UUID gameID) {
-        return new TicTacToeGame(gameID, "1", "2", new TicTacToeBoard());
+        return dao.selectGameByID(gameID).orElseThrow(() -> new TicTacToeException(HttpStatus.NOT_FOUND, String.format("Game %s does not exist.", gameID)));
     }
 
     public TicTacToeGame submitMove(UUID gameID, TicTacToeSubmitMoveRequest moveRequest) {
